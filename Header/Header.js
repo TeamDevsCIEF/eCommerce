56c181e76a7910b5633b6a10a8d956580ac58c9d
Aferebay <87278806+Aferebay@users.noreply.github.com>
2024-06-26 18:08:15 +0200

    Mejora header (cambio auto texto span)

--- Header/Header.js
+++ Header/Header.js
@@ -4,6 +4,11 @@ class MyHeader extends HTMLElement {
 
     // Crear un shadow root
     this.attachShadow({ mode: 'open' });
+
+    // Inicializar variables
+    this.palabras = ["iPhone", "Tablet", "Celulares", "Android"];
+    this.index = 0;
+    this.intervalId = null;
   }
 
   connectedCallback() {
@@ -26,12 +31,50 @@ class MyHeader extends HTMLElement {
         const wrapper = document.createElement('div');
         wrapper.innerHTML = html;
         this.shadowRoot.appendChild(wrapper);
+
+        // Inicializar elementos y eventos
+        this.searchLabelSpan = this.shadowRoot.querySelector('.searchboxForm__label span');
+        this.initEvents();
       } else {
         console.error('Error loading header.html:', response.statusText);
       }
     } catch (error) {
       console.error('Error fetching header.html:', error);
     }
+  }
+
+  initEvents() {
+    // Actualizar el label de búsqueda periódicamente
+    this.updateLabel();
+    this.intervalId = setInterval(() => this.updateLabel(), 5000);
+
+    // Manejar eventos de entrada en el input de búsqueda
+    this.shadowRoot.querySelector('.searchboxForm').addEventListener('input', () => {
+      const input = this.shadowRoot.querySelector('.SearchBox__input');
+      if (input.value) {
+        clearInterval(this.intervalId);
+      } else {
+        clearInterval(this.intervalId);
+        this.updateLabel();
+        this.intervalId = setInterval(() => this.updateLabel(), 5000);
+      }
+    });
+  }
+
+  disconnectedCallback() {
+    // Limpiar el intervalo al desconectar el elemento
+    clearInterval(this.intervalId);
+  }
+
+  updateLabel() {
+    this.searchLabelSpan.style.opacity = 0;
+
+    setTimeout(() => {
+      this.searchLabelSpan.textContent = this.palabras[this.index];
+      this.searchLabelSpan.style.opacity = 1;
+
+      this.index = (this.index + 1) % this.palabras.length;
+    }, 300); // Duración de la transición en ms
   }
 }
 
